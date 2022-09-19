0714dc64c9eea0b1548691db19a483216d3d9eba
Adrian <rodriguezcadr@gmail.com>
2022-09-19 20:32:15 -0300

    limit updater

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const init = () => {
     $playBtn.addEventListener("click", () => {
         weArePlaying = true;
+        playNow();
     });
     $greaterBtn.addEventListener("click", () => {
         if (weArePlaying) {
@@ -19,8 +20,23 @@ const init = () => {
         }
     });
 };
-const playNow = () => {};
-const renderError = () => {};
-const changeLowerLimit = () => {};
-const changeUpperLimit = () => {};
+const playNow = () => {
+    $error.style.display = "none";
+    $theNumber.textContent = `${getNumber()}`;
+};
+const getNumber = () => {
+    const min = Math.ceil(limits[0]);
+    const max = Math.floor(limits[1]);
+    const myNumber = Math.floor(Math.random() * (max - min + 1) + min);
+    return myNumber;
+};
+const renderError = () => {
+    $error.style.display = "block";
+};
+const changeLowerLimit = () => {
+    limits[1] = +$theNumber.textContent;
+};
+const changeUpperLimit = () => {
+    limits[0] = +$theNumber.textContent;
+};
 init();
